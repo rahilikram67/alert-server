04b708f8985fa747cacb5e5a33b92f8dd9361246
adeelikram <rahil.ikram67@gmail.com>
2022-09-28 20:34:05 +0500

    M

--- src/commands/view.ts
+++ src/commands/view.ts
@@ -1,26 +1,30 @@
 import { EmbedBuilder, Message } from "discord.js"
-import { isEmpty } from "lodash"
+import { chunk, isEmpty } from "lodash"
 import { messageSendError } from "../utils/errors"
 
 
 export async function view(message: Message, config: Config) {
-    const { urls,_403 } = config
-    
-    
-    config.urls && config.urls.length && message.channel.send({
-        embeds: [
-            new EmbedBuilder()
-                .setColor(0x0099FF)
-                .setAuthor({ name: "Urls", iconURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-zMtEgd5Lava93Sl4SEEiST2GB-L5DdElsg&usqp=CAU" })
-                .setTitle('Alert Bot Config')
-                .setImage("https://www.freeiconspng.com/thumbs/alert-icon/alert-icon-alert-icon-12.jpg")
-                .setTimestamp()
-                .setDescription(`${urls.length ? urls.join("\n\n") : "\u200b"}`)
-        ]
-    }).catch(err => messageSendError(message))
-    // channels mapping
+    const { urls, _403, channelMap } = config
 
-    !isEmpty(config.channelMap) && message.channel.send({
+
+    if (urls && urls.length) {
+        const embeds: EmbedBuilder[] = []
+        for (let url of chunk(urls)) {
+            embeds.push(
+                new EmbedBuilder()
+                    .setColor(0x0099FF)
+                    .setAuthor({ name: "Urls", iconURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-zMtEgd5Lava93Sl4SEEiST2GB-L5DdElsg&usqp=CAU" })
+                    .setTitle('Alert Bot Config')
+                    .setImage("https://www.freeiconspng.com/thumbs/alert-icon/alert-icon-alert-icon-12.jpg")
+                    .setTimestamp()
+                    .setDescription(`${url?.length ? url.join("\n\n") : "\u200b"}`)
+            )
+
+        }
+        message.channel.send({ embeds }).catch(err => messageSendError(message))
+    }
+
+    !isEmpty(channelMap) && message.channel.send({
         embeds: [
             new EmbedBuilder()
                 .setColor(0x0099FF)
@@ -29,7 +33,7 @@ export async function view(message: Message, config: Config) {
                 .setImage("https://www.freeiconspng.com/thumbs/alert-icon/alert-icon-alert-icon-12.jpg")
                 .setTimestamp()
                 .setDescription(`
-                  ${Object.entries(config.channelMap).map(e => `${e[0]}: ${e[1]}`).join("\n")}
+                  ${Object.entries(channelMap).map(e => `${e[0]}: ${e[1]}`).join("\n")}
                 `)
         ]
     }).catch(err => messageSendError(message))
@@ -48,6 +52,6 @@ export async function view(message: Message, config: Config) {
         ]
     }).catch(err => messageSendError(message))
 
-    if(!_403 && isEmpty(config.channelMap) && config.urls && config.urls.length) message.channel.send("No Entries")
+    if (!_403 && isEmpty(channelMap) && urls && urls.length) message.channel.send("No Entries")
 }
 
