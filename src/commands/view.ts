9b5ab2c0e00ba615e05698193edd153da02c56ca
adeelikram <rahil.ikram67@gmail.com>
2022-09-28 20:54:16 +0500

    M

--- src/commands/view.ts
+++ src/commands/view.ts
@@ -8,20 +8,16 @@ export async function view(message: Message, config: Config) {
 
 
     if (urls && urls.length) {
-        const embeds: EmbedBuilder[] = []
-        for (let url of chunk(urls,20)) {
-            embeds.push(
-                new EmbedBuilder()
+        for (let url of chunk(urls, 20)) {
+            message.channel.send({
+                embeds: [new EmbedBuilder()
                     .setColor(0x0099FF)
                     .setAuthor({ name: "Urls", iconURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-zMtEgd5Lava93Sl4SEEiST2GB-L5DdElsg&usqp=CAU" })
                     .setTitle('Alert Bot Config')
-                    .setImage("https://www.freeiconspng.com/thumbs/alert-icon/alert-icon-alert-icon-12.jpg")
                     .setTimestamp()
-                    .setDescription(`${url?.length ? url.join("\n\n") : "\u200b"}`)
-            )
-
+                    .setDescription(`${url?.length ? url.join("\n\n") : "\u200b"}`)]
+            }).catch(err => messageSendError(message))
         }
-        message.channel.send({ embeds }).catch(err => messageSendError(message))
     }
 
     !isEmpty(channelMap) && message.channel.send({
@@ -30,7 +26,6 @@ export async function view(message: Message, config: Config) {
                 .setColor(0x0099FF)
                 .setAuthor({ name: "Notification Location", iconURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-zMtEgd5Lava93Sl4SEEiST2GB-L5DdElsg&usqp=CAU" })
                 .setTitle('Alert Bot Config')
-                .setImage("https://www.freeiconspng.com/thumbs/alert-icon/alert-icon-alert-icon-12.jpg")
                 .setTimestamp()
                 .setDescription(`
                   ${Object.entries(channelMap).map(e => `${e[0]}: ${e[1]}`).join("\n")}
